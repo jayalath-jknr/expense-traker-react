4cb618d3652f13d4e8d9387e0dcb49c3c5074dbe
Nirasha <jkwapcnrj@gmail.com>
2022-05-19 22:32:19 +0530

    add functionality 2

--- src/components/AddTransaction.js
+++ src/components/AddTransaction.js
@@ -1,14 +1,28 @@
-import React, { useState } from 'react'
+import React, { useContext, useState } from 'react'
+import { GlobalContext } from '../context/GlobleState';
 
 export default function AddTransaction() {
-
+    
     const [text, settext] = useState('');
     const [amount, setamount] = useState(0);
+
+    const { addTransaction } = useContext(GlobalContext);
+    
+    const onSubmit = e => {
+        e.preventDefault();
+        const newTransaction={
+        id:Math.floor(Math.random()*100000000),
+  text,
+  amount:+amount
+        }
+        addTransaction(newTransaction)
+    }
+ 
   return (
     <div> <h3>Add new transaction</h3>
-      <form >
+      <form onSubmit={onSubmit}>
         <div className="form-control">
-          <label htmlFor="text">Text</label>
+          <label htmlFor="text">Transacton Name</label>
           <input type="text" value={text} onChange={(e)=> settext(e.target.value)} placeholder="Enter text..." />
         </div>
         <div className="form-control">
--- src/components/TransactionList.js
+++ src/components/TransactionList.js
@@ -1,12 +1,17 @@
-import React from 'react'
+import React, { useContext } from 'react'
+import { GlobalContext } from '../context/GlobleState'
+import Transaction from './Transaction'
 
 export default function TransactionList() {
+    
+    const {transactions} = useContext(GlobalContext)
   return (
     <div><h3>History</h3>
-      <ul id="list" className="list">
-         <li className="minus">
+          <ul className="list">
+              {transactions.map(transaction => (<Transaction key={transaction.id} transaction={ transaction}/>))}
+         {/* <li className="minus">
           Cash <span>-$400</span><button className="delete-btn">x</button>
-        </li>
+        </li> */}
       </ul></div>
   )
 }
